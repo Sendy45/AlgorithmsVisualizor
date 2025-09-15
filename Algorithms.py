61e62ef3a2d5a7f1aaea08b44519dd98767e9653
Sendy45 <itamarsan45@gmail.com>
2025-09-15 19:32:33 +0300

    insertion sort and sounds

--- Algorithms.py
+++ Algorithms.py
@@ -1,18 +1,38 @@
 import time
 import random
-from Visualization import draw
 
-def bubble_sort(arr: list) -> list:
+from Visualization import draw
+import pygame.midi
+import pygame
+
+def analyze_algorithm(func,  *args, visualize: bool = True, delay: float = 0.02) -> dict:
+    global operation_count
+    operation_count = 0
+
+    start_time = time.perf_counter()
+    result = func(*args, visualize, delay)
+    end_time = time.perf_counter()
+
+    return {
+        "result": result,
+        "time": end_time - start_time,
+        "operations": operation_count
+    }
+
+def bubble_sort(arr: list, visualize: bool, delay: float) -> list:
     n = len(arr)
+    global operation_count
 
     for i in range(n):
 
         swapped = False
         for j in range(0, n-i-1):
 
+            operation_count += 1
+
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
-                draw(arr)
+                draw(arr, screen, delay) if visualize else None
                 swapped = True
 
         if not swapped:
@@ -20,22 +40,45 @@ def bubble_sort(arr: list) -> list:
 
     return arr
 
-def selection_sort(arr: list) -> list:
+def selection_sort(arr: list, visualize: bool, delay: float) -> list:
     n = len(arr)
+    global operation_count
 
     for i in range(n):
         min_idx = i
         for j in range(i, n):
+
+            operation_count += 1
+            player.note_on(note=60, velocity=100)
+
             if arr[j] < arr[min_idx]:
                 min_idx = j
 
         arr[i], arr[min_idx] = arr[min_idx], arr[i]
-        draw(arr)
+        draw(arr, screen, delay) if visualize else None
 
     return arr
 
-def insertion_sort(arr: list) -> list:
+def insertion_sort(arr: list, visualize: bool, delay: float) -> list:
     n = len(arr)
+    global operation_count
+
+    for i in range(1, n):
+        key = arr[i]
+        current_idx = i
+        while current_idx!=0 and key < arr[current_idx-1]:
+            operation_count += 1
+            player.note_on(note=60, velocity=100)
+
+            arr[current_idx] = arr[current_idx-1]
+            draw(arr, screen, delay) if visualize else None
+            current_idx -= 1
+
+        arr[current_idx] = key
+        draw(arr, screen, delay) if visualize else None
+
+    return arr
+
 
 def random_array(length: int) -> list:
     arr = list(range(length))
@@ -43,9 +86,26 @@ def random_array(length: int) -> list:
     return arr
 
 if __name__ == "__main__":
-    arr = random_array(100)
-    start_time = time.time()
-    selection_sort(arr)
-    end_time = time.time()
-    elapsed_time = end_time - start_time
-    print("Elapsed time: ", elapsed_time)
+    # pygame setup
+    pygame.midi.init()
+    pygame.init()
+    screen = pygame.display.set_mode((1280, 720))
+
+    unsorted_arr = random_array(100)
+
+    player = pygame.midi.Output(1)
+    player.set_instrument(11)
+    #player.note_on(note=60, velocity=100)
+    stats = analyze_algorithm(selection_sort, unsorted_arr, delay=0.03)
+    print("time "+ str(stats["time"]))
+    print("operations "+ str(stats["operations"]))
+
+    running = True
+    while running:
+        # poll for events
+        # pygame.QUIT event means the user clicked X to close your window
+        event = pygame.event.wait()
+        if event.type == pygame.QUIT:
+            running = False
+    pygame.quit()
+
--- Visualization.py
+++ Visualization.py
@@ -9,48 +9,21 @@ running = True
 FPS = 3
 arr = [5, 2, 3, 1, 8, 4, 2]
 
-def run():
-    while running:
-        # poll for events
-        # pygame.QUIT event means the user clicked X to close your window
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
+def draw(arr: list, screen, delay: float) -> None:
 
-        # fill the screen with a color to wipe away anything from last frame
-        screen.fill("white")
+    screen.fill("black")
 
-        rec_width = screen.get_width() / (2 * len(arr))
-        rec_height = screen.get_height() / (2 * len(arr))
+    size = 1
 
-        for i, item in enumerate(arr):
-            rec = pygame.Rect(0, i * rec_height, rec_width * item, rec_height)
-            pygame.draw.rect(screen, pygame.Color('blue'), rec)
-
-
-        # RENDER YOUR GAME HERE
-
-        # flip() the display to put your work on screen
-        pygame.display.flip()
-
-        clock.tick(FPS)  # limits FPS
-
-    pygame.quit()
-
-def draw(arr: list) -> None:
-    # pygame setup
-    pygame.init()
-    screen = pygame.display.set_mode((1280, 720))
-    screen.fill("white")
     rec_width = screen.get_width() / (2 * len(arr))
-    rec_height = screen.get_height() / (2 * len(arr))
+    rec_height = screen.get_height() / (size * len(arr))
 
     for i, item in enumerate(arr):
         rec = pygame.Rect(0, i * rec_height, rec_width * item, rec_height)
-        pygame.draw.rect(screen, pygame.Color('blue'), rec)
+        pygame.draw.rect(screen, pygame.Color('green'), rec)
 
     # RENDER YOUR GAME HERE
 
     # flip() the display to put your work on screen
     pygame.display.flip()
-    time.sleep(0.01)
+    time.sleep(delay)
